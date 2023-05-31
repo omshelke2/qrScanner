c9f1b52100c3db6c42c5ab6623939e46dc285584
Omkar Shelke <omshelke0245@gmail.com>
2023-05-31 17:20:50 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-qrValue =  document.getElementById('result').innerHTML;
-
-if (qrValue == "QR Code Value: https://omshelke2.github.io/connect/"){
-    alert("300 rupees Deducted !!");
-    console.log("same")
-} else{
-    alert("Not")
-    console.log("different")
-}
