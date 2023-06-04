4855c9d006fa96443a1b0262c3e3d45fcd961a8e
Omkar Shelke <omshelke0245@gmail.com>
2023-06-04 11:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
- // Create a new instance of the Instascan scanner
- let scanner = new Instascan.Scanner({
-    video: document.getElementById('preview'),
-    mirror: false // Do not mirror the video feed
-  });
+// Create a new instance of the Instascan scanner
+let scanner = new Instascan.Scanner({
+  video: document.getElementById('preview'),
+  mirror: false // Do not mirror the video feed
+});
 
-  // Add an event listener to scan QR codes
-  scanner.addListener('scan', function(content) {
-    document.getElementById('result').innerHTML = 'QR Code Value: ' + content;
+// Add an event listener to scan QR codes
+scanner.addListener('scan', function(content) {
+  document.getElementById('result').innerHTML = 'QR Code Value: ' + content;
 
-    qrValue =  document.getElementById('result').innerHTML;
+  let qrValue = content.trim();
+  let expectedQRValue = 'https://omshelke2.github.io/connect/';
 
-    if (qrValue == "QR Code Value: https://omshelke2.github.io/connect/"){
-      alert("300 rupees Deducted !!");
-      console.log("same");
-    } else{
-      alert("Not");
-      console.log("different");
-    }
-  });
+  if (qrValue === expectedQRValue) {
+    let password = prompt('Enter Password:');
+    if (password === '1234') {
+      alert('300 rupees Deducted !!');
 
-  // Start the scanner
-  Instascan.Camera.getCameras().then(function(cameras) {
-    if (cameras.length > 0) {
-      // Use the back camera if available
-      let backCamera = cameras.find(function(camera) {
-        return camera.name.toLowerCase().includes('back');
-      });
-      if (!backCamera) {
-        backCamera = cameras[0]; // Use the first camera as a fallback
-      }
-      scanner.start(backCamera);
+      // Update account balance
+      let accountBalance = parseFloat(localStorage.getItem('accountBalance'));
+      accountBalance -= 300;
+      localStorage.setItem('accountBalance', accountBalance.toFixed(2));
+
+      // Redirect to account details page
+      window.location.href = 'index.html';
     } else {
-      console.error('No cameras found.');
+      alert('Incorrect Password');
     }
-  }).catch(function(e) {
-    console.error(e);
-  });
+  } else {
+    alert('Invalid QR Code');
+  }
+});
+
+// Start the scanner
+Instascan.Camera.getCameras().then(function(cameras) {
+  if (cameras.length > 0) {
+    let backCamera = cameras.find(function(camera) {
+      return camera.name.toLowerCase().includes('back');
+    });
+    if (!backCamera) {
+      backCamera = cameras[0];
+    }
+    scanner.start(backCamera);
+  } else {
+    console.error('No cameras found.');
+  }
+}).catch(function(e) {
+  console.error(e);
+});
